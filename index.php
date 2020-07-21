800b578b0dc15830f34bbe9ba4921665eeb59528
janioalbuquerque <aurelio.janio@gmail.com>
2020-07-20 22:19:34 -0300

    Subindo Projeto 06 do WebMaster!

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet">
+	<title>Projeto 06</title>
+</head>
+<body>
+	
+	
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+			<div class="icons-social">
+		<a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		
+		</div><!--icons-social-->
+		<div class="clear"></div>
+			<div class="logo">
+				LogoAqui
+			</div><!--logo-->
+			
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--DESKTOP-->
+
+				<nav class="mobile">
+					<i class="fa fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--DESKTOP-->
+				<div class="clear"></div>
+			
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao nosso <span>Website</span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex. Mauris auctor scelerisque mi quis fermentum. In consectetur finibus ligula vitae porttitor. Vestibulum elementum cursus iaculis. Nunc finibus, lectus id tempus porttitor, orci nunc sodales dolor, nec maximus mauris turpis sed erat. Aenean id suscipit ex. Quisque enim massa, egestas at odio ac, accumsan condimentum ex. Donec tempor orci sit amet consequat blandit. Phasellus quis dui orci. Sed auctor est tincidunt pretium maximus.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex. Mauris auctor scelerisque mi quis fermentum. In consectetur finibus ligula vitae porttitor. Vestibulum elementum cursus iaculis. Nunc finibus, lectus id tempus porttitor, orci nunc sodales dolor, nec maximus mauris turpis sed erat. Aenean id suscipit ex. Quisque enim massa, egestas at odio ac, accumsan condimentum ex. Donec tempor orci sit amet consequat blandit. Phasellus quis dui orci. Sed auctor est tincidunt pretium maximus.</p>
+				<a href="">Saiba Mais</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+		
+	</section><!--main-->
+
+	<section class="planos">
+		<div class="chamada-planos">
+		<div class="center">
+			<h2>Confira Nossos Planos</h2>
+		</div><!--center-->
+		</div><!--chamada-planos-->
+		<section class="box-planos">
+			<div class="center">
+				
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$380,00</h2>
+					</div><!--PRECO-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-sigle-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$420,00</h2>
+					</div><!--PRECO-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-sigle-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor</p>
+						
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--PRECO-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-sigle-->
+
+
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section>
+
+	<section class="parallax">
+		<div class="overlay-parallax">
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex. Mauris auctor scelerisque mi quis fermentum. In consectetur finibus ligula vitae porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex. Mauris auctor scelerisque mi quis fermentum. In consectetur finibus ligula vitae porttitor. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex.</p>
+			
+		</div><!--center-->
+	</div><!--overlay-parallax-->
+		
+	</section><!--parallax-->
+
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div>Entre em contato!</h2>
+				<form action="">
+					<input type="text" placeholder="Insira seu nome">
+					<input type="email" placeholder="Insira seu E-mail">
+					<textarea placeholder="Sua Mensagem.." name="" id="" cols="30" rows="10"></textarea>
+					<input type="submit" value="Enviar">
+
+				</form>
+			</div>
+
+			<div class="w50 time">
+
+				<h2><div class="borda-efeito"></div>Conheçanosso time!</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><!--box-avatar-->
+					<div class="info-usuario">
+						<p style="font-weight: bold;">Janio Albuquerque</p>
+						<p style="font-weight: italic;">Front-End Desenveloper</p>
+					</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">	
+						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex.	</p>
+					</div><!--descricao-usuario-box-->
+
+					
+				</div>
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><!--box-avatar-->
+					<div class="info-usuario">
+						<p style="font-weight: bold;">Janio Albuquerque</p>
+						<p style="font-weight: italic;">Front-End Desenveloper</p>
+					</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">	
+						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut viverra nisi. Etiam ligula justo, volutpat sed cursus ultrices, placerat at ipsum. Proin velit massa, tempor eu orci at, ullamcorper aliquam ex.	</p>
+					</div><!--descricao-usuario-box-->
+
+					
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div><!--center-->
+	</section>
+
+	<footer>
+		<p>Todos os direitos Resevardos a Janio Albuquerque</p>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script>
+		$(function() {
+			$('nav.mobile i').click(function(){
+				var el = $(this).parent().find('ul');
+				if(el.is(':visible') == false){
+					el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			})
+
+		})
+	</script>
+	
+
+</body>
+</html>
